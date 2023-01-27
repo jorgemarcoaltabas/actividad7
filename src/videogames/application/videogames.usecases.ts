ac60a3eb8601fcbd66af9f4046afcfad5ea0729e
Tomas Casa WIn <210099@fppiramide.com>
2023-01-27 01:09:35 +0100

    s

--- src/videogames/application/videogames.usecases.ts
+++ src/videogames/application/videogames.usecases.ts
@@ -1,28 +1,33 @@
 import Videogame from "../domain/Videogame";
 import VideogamesRepository from "../domain/Videogame.repository";
+import { isAuth } from "../../context/security/auth";
 
-export default class VideogamesUseCases{
+export default class VideogamesUseCases {
     videogamesRepository: VideogamesRepository;
 
-    constructor(videogamesRepository: VideogamesRepository){
+    constructor(videogamesRepository: VideogamesRepository) {
         this.videogamesRepository = videogamesRepository;
     }
 
-    async getAllGames() : Promise<Videogame[]>{
-        const videogames : Videogame[] = await this.videogamesRepository.getAllGames();
+    async getAllGames(): Promise<Videogame[]> {
+        const videogames: Videogame[] = await this.videogamesRepository.getAllGames();
 
         return videogames;
     }
 
-    async getOneGame (id : Number): Promise<Videogame>{
-        const videogame : Videogame = await this.videogamesRepository.getOneGame(id);
+    async getOneGame(id: Number): Promise<Videogame> {
+        const videogame: Videogame = await this.videogamesRepository.getOneGame(id);
         return videogame;
     }
 
-    async addGame( videogame : Videogame) : Promise<Videogame>{
-        
+    async addGame(videogame: Videogame): Promise<Videogame> {
+
         return this.videogamesRepository.addGame(videogame);
     }
 
-    
+    async modifyGame(videogame: Videogame, id: Number) {
+        return this.videogamesRepository.modifyGame(videogame, id);
+    }
+
+
 }
--- src/videogames/infrastructure/rest/videogames.router.ts
+++ src/videogames/infrastructure/rest/videogames.router.ts
@@ -3,34 +3,42 @@ import VideogamesUseCases from '../../application/videogames.usecases';
 import Videogame from '../../domain/Videogame'
 import VideogamesRepository from '../../domain/Videogame.repository';
 import VideogamesRepositoryPostgres from '../db/videogames.repository.postgres';
+import { isAuth } from '../../../context/security/auth';
 
 const router = express.Router();
-const videogamesRepository : VideogamesRepository = new VideogamesRepositoryPostgres();
-const videogamesUseCases : VideogamesUseCases = new VideogamesUseCases(videogamesRepository)
+const videogamesRepository: VideogamesRepository = new VideogamesRepositoryPostgres();
+const videogamesUseCases: VideogamesUseCases = new VideogamesUseCases(videogamesRepository)
 
-router.get("/", async (req:Request , res: Response) => {
-    try{
-        const videogames : Videogame[] = await videogamesUseCases.getAllGames();
-        res.send(videogames)
-    }catch(error){
+router.get("/", async (req: Request, res: Response) => {
+    try {
+        const videogames: Videogame[] = await videogamesUseCases.getAllGames();
+        res.json(videogames)
+    } catch (error) {
         console.error(error)
     }
 })
 
-router.post("/add",async (req:Request, res: Response) => {
-    try{
-        const videogame : Videogame = {
-            name : req.body.name,
-            price : req.body.price,
+router.get("/:id", async (req: Request, res: Response) => {
+    try {
+        const videogame: Videogame = await videogamesRepository.getOneGame(Number(req.params.id));
+        res.json(videogame)
+    }
+})
+
+router.post("/add", isAuth, isAdmin, async (req: Request, res: Response) => {
+    try {
+        const videogame: Videogame = {
+            name: req.body.name,
+            price: req.body.price,
         }
 
-        const newVideogame : Videogame = await videogamesUseCases.addGame(videogame);
+        const newVideogame: Videogame = await videogamesUseCases.addGame(videogame);
 
         res.send(newVideogame);
-    }catch(error){
+    } catch (error) {
         console.error(error);
     }
 })
 
 
-export {router as routerVideogames}
+export { router as routerVideogames }
